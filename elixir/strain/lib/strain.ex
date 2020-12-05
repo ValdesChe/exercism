0a4440e24a28310b55622890e0b0646636cca49f
dorval <valdesche03@gmail.com>
2020-12-06 00:31:37 +0100

    Solved Strain problem. keep and discard

---
+++ elixir/strain/lib/strain.ex
@@ -0,0 +1,30 @@
+defmodule Strain do
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep(list, fun) do
+    list
+    |> Enum.reduce([],  fn number, acc ->
+      if apply(fun, [number]) do acc ++ [number] else acc end
+    end)
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard(list, fun) do
+    list
+    |> Enum.reduce([],  fn number, acc ->
+      if !apply(fun, [number]) do acc ++ [number] else acc end
+    end)
+  end
+
+end
